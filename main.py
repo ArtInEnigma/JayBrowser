03a708e350f36ffb5aaf64a16ade12efecfcdeb8
ArtInEnigma <122215464+ArtInEnigma@users.noreply.github.com>
2023-01-16 17:48:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtCore import QUrl, Qt, QSize
 from PyQt5.QtGui import QIcon, QGuiApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLineEdit, QVBoxLayout, QWidget,QToolButton, QTabWidget)
+from PyQt5.QtWidgets import (QApplication, QSizePolicy,QHBoxLayout, QLineEdit, QVBoxLayout, QWidget,QToolButton, QTabWidget)
 
 class Browser(QWidget):
     def __init__(self):
@@ -15,45 +15,56 @@ class Browser(QWidget):
         self.search_bar.setPlaceholderText("Search Google or type URL")
         self.search_bar.setFixedWidth(575)
         self.search_bar.setFixedHeight(30)
-        self.search_bar.setStyleSheet("margin: 5px 3px; height: 35px; width: 120px color: #5c5c5c; border: none; background-color: #f2f2f; font-weight: bold; ")
         self.search_bar.setAlignment(Qt.AlignCenter)
+
 
         # Create the tab widget
         self.tab_widget = QTabWidget(self)
         self.tab_widget.setTabsClosable(True)
         self.tab_widget.tabCloseRequested.connect(self.close_tab)
 
+        # Create the horizontal layout for the search bar and toolbar
+        self.toolbar_searchbar_layout = QHBoxLayout()
+
         # Create the tool bar
         self.tool_bar = QHBoxLayout()
+        self.tool_bar.setAlignment(Qt.AlignLeft)
 
         # Create the back button
         self.back_button = QToolButton(self)
         self.back_button.setIcon(QIcon("back.png"))
+        self.back_button.setIconSize(QSize(24, 24))
         self.back_button.clicked.connect(self.go_back)
         self.tool_bar.addWidget(self.back_button)
 
         # Create the forward button
         self.forward_button = QToolButton(self)
         self.forward_button.setIcon(QIcon("forward.png"))
+        self.forward_button.setIconSize(QSize(24, 24))
         self.forward_button.clicked.connect(self.go_forward)
         self.tool_bar.addWidget(self.forward_button)
 
         # Create the reload button
         self.reload_button = QToolButton(self)
         self.reload_button.setIcon(QIcon("reload.png"))
+        self.reload_button.setIconSize(QSize(24, 24))
         self.reload_button.clicked.connect(self.reload_page)
         self.tool_bar.addWidget(self.reload_button)
 
         # Create the new tab button
         self.new_tab_button = QToolButton(self)
         self.new_tab_button.setIcon(QIcon("newtab.png"))
+        self.new_tab_button.setIconSize(QSize(24, 24))
         self.new_tab_button.clicked.connect(self.new_tab)
         self.tool_bar.addWidget(self.new_tab_button)
 
+        # Add the search bar and toolbar to the horizontal layout
+        self.toolbar_searchbar_layout.addLayout(self.tool_bar)
+        self.toolbar_searchbar_layout.addWidget(self.search_bar)
+        
         # Create the layout
         layout = QVBoxLayout(self)
-        layout.addLayout(self.tool_bar)
-        layout.addWidget(self.search_bar,0,Qt.AlignHCenter)
+        layout.addLayout(self.toolbar_searchbar_layout)
         layout.addWidget(self.tab_widget)
 
         # Connect the search bar to the web view
@@ -88,6 +99,7 @@ class Browser(QWidget):
             url = "https://www.google.com/search?q=" + url
         web_view = self.tab_widget.currentWidget()
         web_view.setUrl(QUrl(url))
+
 
     def go_back(self):
         web_view = self.tab_widget.currentWidget()
